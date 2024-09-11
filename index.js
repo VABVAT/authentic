9fc6eed7d42f3b405c8cfd7af66637aa0f95b35f
Vaibhav <sidanavaibhav@gmail.com>
2024-09-11 10:40:18 +0530

    try2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = new express();
+const jwt = require("jsonwebtoken");
+
 const JWT_SECRET = "nfjklgvsdfngvjlkdasd";
 
 app.use(express.json);
@@ -7,6 +9,7 @@ app.use(express.json);
 var db = [];
 
 app.get("/",(req, res) => {
-
+    res.sendFile(__dirname + '/public/index.html')
 })
 
+app.listen(3000)
