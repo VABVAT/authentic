42d2f87bc9a032a58dd224d85d53cdad7467ac19
Vaibhav <sidanavaibhav@gmail.com>
2024-09-11 17:44:04 +0530

    firstDraft

--- READEME.md
+++ READEME.md
@@ -4,3 +4,9 @@ create a fullstack
 2. host html on backend as well
 3. use backend authentication logic similar to done in week6
 4. HINTS -> localstorage.get, localstorage.set, res.sendfile(__dirname), jwt.verify, jwt.sign , await fetch
+5. token set needs to be done in signin and also replaced in local storage
+
+
+important points
+1. local storage is accesed through frontend only
+2. jwt token is always different even for same user and password
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = new express();
 const jwt = require("jsonwebtoken");
-
+const cors = require("cors");
 const JWT_SECRET = "nfjklgvsdfngvjlkdasd";
 
 //! another type of middleware for css files (express.static('public'))->shows all files in the public folder
+//! more than one HTML pages ............... use cors() as using / i was able to host index on same port  but not other pages
+
+app.use(cors());
+
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -14,6 +18,11 @@ app.get("/",(req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
+//* When a user logs in, they provide credentials to the server, which verifies them against the database.
+//* If the credentials are correct, the server generates a token and sends it back to the client.
+//* For subsequent requests, the client sends the token in the request headers (or other locations). 
+//* The server can validate the token (checking its signature and expiration) and extract user information from it without 
+//* needing to query the database again.
 function verifier(req, res, next){
     const username = req.body.username;
     const password = req.body.password;
@@ -41,14 +50,28 @@ app.post("/signin", verifier ,(req, res)=>{
     res.status(200).send(token);
 })
 
-app.get("/me", (req, res)=>{
-    const token = localStorage.getItem("token");
-    if(token){
-       const decoder = jwt.verify(token, JWT_SECRET);
-       res.status(200).send(decoder.user); 
+app.post("/addTodo", (req, res)=>{
+    //! local storage does not exists in node or express it can only be accesed from frontEnd
+    
+    
+    const token = req.headers.token;
+    //! token is not stored in data base
+    const user = jwt.verify(token, JWT_SECRET);
+    if(user){
+        const name = user.user;
+        const obj = db.find((ob)=>{
+            return ob.username === name;
+        })
+        if(!obj.tasks) obj.tasks = [];
+        if(req.body.task_){
+        obj.tasks.push(req.body.task_);
+        }
+        res.status(200).json(obj.tasks);
     }else{
-        res.status(400).send("wrong");
+        res.status(401).send("Unauthorized");
     }
+
 })
+
 
 app.listen(3000)
