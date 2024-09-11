6d39353ea4dbefcac2a1b6067d88e0f4831ff67f
Vaibhav <sidanavaibhav@gmail.com>
2024-09-11 10:06:55 +0530

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = new express();
+const JWT_SECRET = "nfjklgvsdfngvjlkdasd";
+
+app.use(express.json);
+
+var db = [];
+
+app.get("/",(req, res) => {
+
+})
+
